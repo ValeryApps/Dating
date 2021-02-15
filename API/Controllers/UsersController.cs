2f6a69de9e43690cc5a1e9e56c799edd9da4970e
ValeryApps <valeryghn10@outlook.com>
2021-02-15 23:13:38 +0000

    Squeleton

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using api.entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController:ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ API/Entities/AppRole.cs
@@ -0,0 +1,10 @@
+﻿﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace api.entities
+{
+    public class AppRole:IdentityRole<int>
+    {
+        public ICollection<AppUserRole> AppUserRoles { get; set; }
+    }
+}
